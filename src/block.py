ca9623a9add3a0046cbe3ba3aac1b9e1453e5791
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-05-27 11:30:39 +0200

    add fundamental architecture

---
+++ src/block.py
@@ -0,0 +1,31 @@
+from typing import List
+import hashlib
+
+from transaction import Transaction
+
+class Block:
+    def __init__(self, index: int, timestamp: int, transactions: List[Transaction], previous_hash:str) :
+        self.index = index
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = 0
+        self.hash = self.calculateHash(self)
+
+    def calculateHash(self) -> str:
+        ## Calculate the hash of the block
+        block_string = str(self.index) + str(self.timestamp) + str(self.transactions) + str(self.previous_hash) + str(self.nonce)
+        return hashlib.sha256(block_string.encode()).hexdigest()
+    
+    def mineBlock(self, difficulty: int) :
+        target = "0" * difficulty
+        while self.hash[0:difficulty] != target:
+            self.nonce += 1
+            self.hash = self.calculateHash()
+
+    def hasValidTransactions(self) -> bool:
+        ## Check if all transactions in the block are valid
+        for transaction in self.transactions:
+            if not transaction.isValid():
+                return False
+        return True
---
+++ src/transaction.py
@@ -0,0 +1,26 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ed25519
+
+# sender: hexadecimal representation of the public key of the sender
+# recipient: hexadecimal representation of the public key of the recipient
+# amount: amount of coins to be transferred
+# signature: hexadecimal representation of the signature of the transaction
+class Transaction:
+    def __init__(self, sender: str, recipient: str, amount: int, signature: str) :
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
+        self.signature = signature
+    
+    def isValid(self) -> bool:
+        sender_public_key = bytes.fromhex(self.sender)
+        signature_in_bytes = bytes.fromhex(self.signature)
+
+        public_key = ed25519.Ed25519PublicKey.from_public_bytes(sender_public_key)
+
+        try:
+            data = self.sender.encode() + self.recipient.encode() + str(self.amount).encode()
+            public_key.verify(signature_in_bytes, data.encode())
+        except:
+            return False
+        return True
