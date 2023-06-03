8111d3feb54595a53ae561efbaced1b99265977c
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-06-03 14:40:31 +0200

    check validity of transaction, receive transaction and start controller in a separate thread

--- blockchain/src/blockchain.py
+++ blockchain/src/blockchain.py
@@ -6,6 +6,7 @@ from block import Block
 from memPool import MemPool
 from transaction import Transaction
 from blockchainUI import startUI, printBlock
+from controller import Controller
 
 class Blockchain:
     def __init__(self, chain: List[Block], difficulty: int, mem_pool: MemPool) -> None:
@@ -13,7 +14,7 @@ class Blockchain:
         self.difficulty = difficulty
         self.mem_pool = mem_pool
 
-    def addBlock(self, transactions: List[Transaction]) :
+    def addBlock(self, transactions: List[Transaction]) -> List[Transaction]:
         if len(self.chain) == 0:
             # Genesis block
             previous_hash = "0"
@@ -21,9 +22,18 @@ class Blockchain:
         else:
             # New block
             previous_block = self.chain[-1]
-            block = Block(previous_block.index + 1, self.get_current_timestamp(), transactions, previous_block.hash)
+
+            ## Remove invalid transactions
+            valid_transactions = []
+            for transaction in transactions:
+                if transaction.isValid():
+                    valid_transactions.append(transaction)
+            
+            ## Create new block
+            block = Block(previous_block.index + 1, self.get_current_timestamp(), valid_transactions, previous_block.hash)
             block.mineBlock(self.difficulty)
         self.chain.append(block)
+        return transactions
 
     def isValid(self) -> bool:
         for i in range(1, len(self.chain)):
@@ -44,8 +54,20 @@ class Blockchain:
         timestamp = int(current_datetime.timestamp())
         return timestamp
     
+    def receiveTransaction(self, transaction: Transaction) -> None:
+        self.mem_pool.addTransaction(transaction)
+    
     def start(self) -> None:
+
+        ## Start controller in a separate thread
+        controller = Controller(self)
+        thread  = threading.Thread(target=controller.run)
+        thread.start()
+        time.sleep(1)
+
+        ## Start blockchain
         startUI()
         while True:
-            self.addBlock(self.mem_pool.getTransactions())
+            added_transactions = self.addBlock(self.mem_pool.getTransactions())
+            self.mem_pool.removeTransactions(added_transactions)                
             printBlock(self.chain[-1])    
