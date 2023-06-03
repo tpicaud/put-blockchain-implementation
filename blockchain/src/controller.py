052dec83bd927c53aeab6571b60c9e9295abfc3e
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-06-03 14:37:52 +0200

    add controller to receive transactions

---
+++ blockchain/src/controller.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from transaction import Transaction
+class Controller:
+    def __init__(self, _blockchain):
+        self.app = Flask(__name__)
+        self.blockchain = _blockchain
+
+        # Register routes
+        self.register_routes()
+
+    def register_routes(self):
+        # Endpoint for receiving new transactions
+        self.app.route('/transactions/new', methods=['POST'])(self.receive_transaction)
+
+    def receive_transaction(self):
+        try:
+            transaction_data = request.get_json()
+            sender_address = transaction_data['sender_address']
+            recipient_address = transaction_data['recipient_address']
+            amount = transaction_data['amount']
+            signature = transaction_data['signature']
+
+            transaction = Transaction(sender_address, recipient_address, amount, signature)
+            self.blockchain.receiveTransaction(transaction)
+
+            return jsonify({'message': 'Transaction received and sent to mempool.'}), 200
+        except:
+            return jsonify({'message': 'Invalid transaction format.'}), 400
+        
+    def run(self):
+        self.app.run()
