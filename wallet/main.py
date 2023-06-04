8660a251dd9c98878b5a3cfba66bba0b7a11908f
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-06-04 13:07:23 +0200

    add main for testing

--- wallet/main.py
+++ wallet/main.py
@@ -4,7 +4,7 @@ def main():
 
     wallet = Wallet()
     wallet.generate_key_pair()
-    wallet.send_transaction("recipient_address_1", 10)
+    wallet.send_transaction("recipient_address_1", 4)
     print("Public key :", wallet.public_key_hex)
 
 if __name__ == "__main__":
